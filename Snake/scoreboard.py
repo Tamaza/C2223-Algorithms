73052d63b90ddbd57c3fa80cf8ef63165df681c6
Irakli Grigolia <33191156+Tamaza@users.noreply.github.com>
2020-12-21 11:21:24 -0500

    Add files via upload

--- Snake/scoreboard.py
+++ Snake/scoreboard.py
@@ -10,6 +10,9 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
+
         self.color("white")
         self.penup()
         self.goto(0,250)
@@ -17,11 +20,11 @@ class ScoreBoard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
 
     def restart_game(self):
@@ -29,17 +32,16 @@ class ScoreBoard(Turtle):
         subprocess.run(["python" ,"main.py"])
         quit()
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game over! Want to restart the game? Y or N", align=ALIGNMENT, font=FONT)
-        answer = input("Y or N?")
-        if answer == "Y":
-            self.restart_game()
-            quit()
-        else:
-            quit()
+
 
 
 
